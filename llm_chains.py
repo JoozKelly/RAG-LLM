a84911c9214ed046a8a82a85d3ff0b0fd81db846
JoozKelly <joozkelly12@gmail.com>
2025-04-17 21:24:56 +0800

    Initial Commit

--- llm_chains.py
+++ llm_chains.py
@@ -4,6 +4,7 @@ from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.prompts import PromptTemplate
+from langchain.llms import LlamaCpp
 from langchain.llms import CTransformers
 from langchain.vectorstores import Chroma
 import chromadb
@@ -12,8 +13,24 @@ import yaml
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-def create_llm(model_path = config["model_path"]["large"], model_type = config["model_type"], model_config = config["model_config"]):
-    llm = CTransformers(model=model_path, model_type=model_type, config=model_config)
+# def create_llm(model_path = config["model_path"]["large"], model_type = config["model_type"], model_config = config["model_config"]):
+#    llm = CTransformers(model=model_path, model_type=model_type, config=model_config)
+#    return llm
+
+def create_llm(model_path = config["model_path"]["large"], model_config = config.get("model_config", {})):
+    llm_params = {}
+    if "gpu_layers" in model_config:
+        llm_params["n_gpu_layers"] = model_config["gpu_layers"]
+    if "context_length" in model_config:
+        llm_params["n_ctx"] = model_config["context_length"]
+    if "temperature" in model_config:
+        llm_params["temperature"] = model_config["temperature"]
+    if "max_new_tokens" in model_config:
+        llm_params["max_tokens"] = model_config["max_new_tokens"]
+    llm_params["model_path"] = model_path
+    llm_params["verbose"] = True  # Good for debugging
+
+    llm = LlamaCpp(**llm_params)
     return llm
 
 def create_embeddings(embeddings_path = config["embeddings_path"]):
